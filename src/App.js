ab7de3ce1a8918873cabc5bb0f21bf74e679e60e
fluturecode <fluturecode@gmail.com>
2020-07-26 22:40:10 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
+import Message from './Message';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    setUsername(prompt('Please enter your name'));
+  }, [])
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, { username: username, text: input }]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Messenger App</h1>
+      <h2>Welcome {username}</h2>
+
+      <form>
+        <FormControl>
+          <InputLabel>Type your message...</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)} />
+          <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button>
+        </FormControl>
+      </form>
+
+      {
+        messages.map(message => (
+          <Message username={username} message={message}/>
+        ))
+      }
     </div>
   );
-}
+};
 
 export default App;
